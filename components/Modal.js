e0a761049b14529d006a09fafda675c76f717e31
David Urrego Zago <daviduzago@gmail.com>
2023-04-22 02:25:09 -0500

    modals wip

--- components/Modal.js
+++ components/Modal.js
@@ -1,10 +1,67 @@
 import React from 'react'
-import {View, Modal} from 'react-native'
+import {View, Modal, StyleSheet, Platform, TouchableOpacity} from 'react-native'
 import Typography from './Typography'
 import Button from './Button'
 import Spacer from '../utils/Spacer'
 import colors from '../utils/colors'
+import {useSafeAreaInsets} from 'react-native-safe-area-context'
 
+export default function ModalComponent(props) {
+    const {
+        children,
+        animationType = 'fade',
+        transparent = true,
+        variant = 'light',
+        onClose
+    } = props
+    const insets = useSafeAreaInsets();
+    return (
+        <Modal
+            animationType={animationType}
+            transparent={transparent}
+            {...props}>
+            <View style={[style.container, {
+                paddingBottom: insets.bottom,
+                backgroundColor: variant === 'light' ? colors.white : colors.grayPrimary
+            }]}>
+                {Platform.OS === "ios" && <View style={style.closingBar}></View>}
+                {children}
+            </View>
+            <TouchableOpacity
+                onPress={props.onRequestClose}
+                style={style.overlay}
+            />
+        </Modal>
+    )
+}
 
-
-
+const style = StyleSheet.create({
+    closingBar: {
+        width: 50,
+        height: 6,
+        backgroundColor: colors.gray3,
+        borderRadius: 50,
+        marginBottom: 8
+    },
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        position: 'absolute',
+        paddingHorizontal: 12,
+        paddingTop: 12,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 9999,
+    },
+    overlay: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+})
--- screens/components/ModalsScreen.js
+++ screens/components/ModalsScreen.js
@@ -3,22 +3,20 @@ import {View} from "react-native";
 import Wrapper from "../../components/ui/wrapper";
 import Spacer from "../../utils/Spacer"
 import Button from "../../components/Button";
-import {queueDisplayModal} from "../../utils/ModalUtils"
 import Typography from "../../components/Typography";
 import Input from "../../components/Input";
 import Select from "../../components/Select"
+import Modal from "../../components/Modal";
 
 function ModalsScreen() {
+    const [visible, setVisible] = React.useState(false)
     return (
         <Wrapper hasTopNav>
             <View style={{flex: 1, padding: 20, width: "100%"}}>
-                <Button title="Default Modal" onPress={e => {
-                    queueDisplayModal({
-                        modal: 'ConfirmModal',
-                        title: 'Confirm Modal',
-                        body: 'Body',
-                    })
-                }} />
+                <Button title="Default Modal" onPress={() => setVisible(!visible)} />
+                <Modal onRequestClose={() => setVisible(!visible)} visible={visible}>
+                    <Button title="Close Modal" onPress={() => setVisible(!visible)} />
+                </Modal>
             </View>
         </Wrapper>
     );

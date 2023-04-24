2caed8f8c26c4c23a8c72a010ca1628dd89bd873
David Urrego <daviduzago@gmail.com>
2023-04-24 15:05:21 -0500

    otp input wip

---
+++ components/otpInput.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {View} from "react-native"
+
+export default function otpInput(props) {
+    return (
+        <View style={{width: "100%", justifyContent: "center", alignItems: "center"}}>
+
+        </View>
+    )
+}

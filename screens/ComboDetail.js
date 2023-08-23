cc9be6e48704e5dde44b8d3ae29d469545d06792
David Urrego Zago <daviduzago@gmail.com>
2023-08-23 15:53:14 -0500

    finished the combo detail screen

--- screens/ComboDetail.js
+++ screens/ComboDetail.js
@@ -14,6 +14,10 @@ function ComboDetails({route, navigation}) {
     const insets = useSafeAreaInsets();
     const item = MOCKED_DATA[0];
 
+    //TODOS:
+    // Add or remove button should be displayed only if the item is already in the cart
+
+    // This will be a reducer
     const handleSelection = (optionType, selectedId) => {
         const selectedOption = item.comboOptions.find(opt => opt.type === optionType).options.find(o => o.id === selectedId);
 
@@ -38,22 +42,19 @@ function ComboDetails({route, navigation}) {
         setOptions(prevOptions => ({...prevOptions, ...newOptions}));
     }, []);
 
-    React.useEffect(() => {
-        console.log(options)
-    }, [options])
-
     return (
         <View style={[styles.container, {paddingTop: insets.top}]}>
+            {/* TOPNAV */}
             <View style={[styles.topNav, {top: insets.top}]}>
                 <Pressable onPress={() => navigation.goBack()}
                     style={{
                         padding: 4,
                         borderRadius: 20,
-                        backgroundColor: colors.grayBg,
+                        backgroundColor: colors.grayPrimary,
                         justifyContent: 'center',
                         alignItems: 'center',
                     }}>
-                    <Icon name="chevron-left" size={26} color="black" />
+                    <Icon name="chevron-left" size={26} color="white" />
                 </Pressable>
                 <Spacer />
                 {/* <Typography color="black" variant="medium">{item.comboTitle}</Typography> */}
@@ -67,9 +68,9 @@ function ComboDetails({route, navigation}) {
                         <Typography variant="body">{item.comboDescription}</Typography>
                         <Spacer />
                         <Typography bold variant="medium">${item.price}</Typography>
-                        <Spacer />
+                        <Spacer x={3} />
                         {item.comboOptions.map((option, index) => (
-                            <>
+                            <React.Fragment key={"combo-option-" + index}>
                                 <ComboOptions
                                     defaultSelection={option.defaultSelection}
                                     label={option.title}
@@ -77,11 +78,30 @@ function ComboDetails({route, navigation}) {
                                     onSelect={(id) => handleSelection(option.type, id)}
                                 />
                                 <Spacer x={2} />
-                            </>
+                            </React.Fragment>
                         ))}
                     </View>
                 </View>
             </ScrollView>
+            {/* BOTNAV */}
+            <View style={[styles.bottomNav, {bottom: 0, paddingBottom: insets.bottom + 20}]}>
+                {/*  TODO: We should fix the button to make it look like the design */}
+                <View style={styles.customButton}>
+                    <Pressable onPress={() => console.log("pressed")}>
+                        <Icon name="trash" size={26} color="black" />
+                    </Pressable>
+                    <Spacer x={2} />
+                    <Typography variant="medium">1</Typography>
+                    <Spacer x={2} />
+                    <Pressable onPress={() => console.log("pressed")}>
+                        <Icon name="plus" size={26} color="black" />
+                    </Pressable>
+                </View>
+                <Spacer x={2} />
+                <View style={{flex: 1}}>
+                    <Button fullWidth variant="primary" title={`Add $${item.price}`} />
+                </View>
+            </View>
         </View>
     )
 }
@@ -91,10 +111,20 @@ export default ComboDetails;
 const styles = StyleSheet.create({
     body: {
         flex: 1,
+        paddingBottom: 200
     },
     container: {
         flex: 1,
         alignItems: 'center',
+    },
+    customButton: {
+        paddingVertical: 16,
+        paddingHorizontal: 20,
+        flexDirection: "row",
+        borderColor: "black",
+        borderRadius: 15,
+        borderWidth: 1,
+        justifyContent: "space-evenly"
     },
     info: {
         flex: 1,
@@ -109,6 +139,15 @@ const styles = StyleSheet.create({
         borderBottomRightRadius: 30,
     },
     topNav: {
+        padding: 20,
+        flexDirection: "row",
+        position: "absolute",
+        alignItems: "center",
+        left: 0,
+        right: 0,
+        zIndex: 100,
+    },
+    bottomNav: {
         padding: 10,
         flexDirection: "row",
         position: "absolute",
@@ -116,5 +155,6 @@ const styles = StyleSheet.create({
         left: 0,
         right: 0,
         zIndex: 100,
+        backgroundColor: colors.grayBg
     }
 })
